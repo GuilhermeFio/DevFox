383a1f8627199ee846b08fee033feb8906ed2526
Giovanna <hesselgiovanna579@gmail.com>
2024-08-15 11:47:56 -0300

    arrumar as sessões e os cartões feiões

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -56,6 +56,36 @@ function Landing() {
         
         <h2 className='texto3'> Aqui ofereçemos serviços de qualidade! Confira abaixo: </h2>
         
+          <div className='cartao'>
+            
+             <div className='cartao1'>
+
+              <div className='imagens1'>
+              <h1>1- Desenvolvimento web</h1>
+             <img src="/assets/images/web.jfif" alt="" />
+              </div>
+              
+               <div className='imagens2'>
+               <h2>2-Cybersegurança</h2>
+             <img src="/assets/images/seguranca.jfif" alt="" />
+               </div>
+             
+             </div>
+
+             <div className='cartao2'>
+
+              <div className='imagens3'> 
+              <h3 className='uxui'>3- UX&UI</h3>
+             <img src="/assets/images/program.jfif" alt="" />
+              </div>
+            
+               <div className='imagens4'>
+               <h4>4- Data Analysis</h4>
+             <img src="/assets/images/grafico.jfif" alt="" />
+               </div>
+          
+             </div>
+          </div>
 
      </section>
 
